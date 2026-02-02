99c64248e8f8f194c3248f878ebb5c32d45a3fe0
mkolojennifer128159-max <mkolojennifer.128159@marwadiuniversity.ac.in>
2026-02-02 10:06:57 +0530

    Add files via upload

---
+++ 2numbersandoperator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double num1, num2;
+        char op;
+
+        Console.Write("Enter first number: ");
+     
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        op = Convert.ToChar(Console.ReadLine());
+
+        if (op == '+')
+        {
+            Console.WriteLine("Result = " + (num1 + num2));
+        }
+        else if (op == '-')
+        {
+            Console.WriteLine("Result = " + (num1 - num2));
+        }
+        else if (op == '*')
+        {
+            Console.WriteLine("Result = " + (num1 * num2));
+        }
+        else if (op == '/')
+        {
+            // 3. CHANGE: Check for Zero before dividing
+            if (num2 != 0)
+            {
+                Console.WriteLine("Result = " + (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine("Error: Cannot divide by zero!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Operator");
+        }
+        Console.ReadLine();
+    }
+}
